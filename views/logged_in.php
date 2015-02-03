59d62704a783afb9c5ef3c3b7e7b2a11b1ea681c
Piotr Górak <piotrgorak93@gmail.com>
2015-02-03 20:15:00 +0100

    Customizing the template
    Login site seems to be ready

--- views/logged_in.php
+++ views/logged_in.php
@@ -1,8 +1,10 @@
 <?php include_once 'header.html';?>
-<!-- if you need user information, just put them into the $_SESSION variable and output them here -->
-Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in.
-Try to close this browser tab and open it again. Still logged in! ;)
+    <div class="container">
 
-<!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
-<a href="index.php?logout">Logout</a>
+      <p>Hello <?php $_SESSION['username']?>!</p>
+        <p><a href="index.php?logout">Logout</a></p>
+
+    </div>
+
+
 <?php include_once 'footer.html';?>
--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -12,19 +12,35 @@ if (isset($login)) {
         }
     }
 }
+include_once 'header.html';
 ?>
 
 <!-- login form box -->
-<form method="post" action="index.php" name="loginform">
 
-    <label for="login_input_username">Username</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
 
-    <label for="login_input_password">Password</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+<div class="container">
 
-    <input type="submit"  name="login" value="Log in" />
 
-</form>
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <form class="form-signin" method="post" action="index.php" name="loginform">
+                        <h2 class="form-signin-heading">Logowanie</h2>
 
-<a href="register.php">Register new account</a>
+                        <label for="login_input_username" class="sr-only" >Username</label>
+                        <input id="login_input_username" class="form-control" placeholder="Login" type="text" name="user_name" required />
+
+                        <label for="login_input_password" class="sr-only" >Password</label>
+                        <input id="login_input_password" class="form-control" placeholder="Password" type="password" name="user_password" autocomplete="off" required />
+                        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Zaloguj się</button>
+
+                    </form>
+                    <p>
+                    <a href="register.php">Zarejestruj sie</a>
+                    </p>
+                </div>
+
+
+</div>
+
+
+<?php include_once 'footer.html';?>
